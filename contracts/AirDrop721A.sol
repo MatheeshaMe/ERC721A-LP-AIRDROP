15715bbc35dc42b0627f26a8882ca8321553aae2
sonicprv <77117024+MatheeshaMe@users.noreply.github.com>
2023-01-07 10:30:44 +0530

    erc721a-merkleproof

---
+++ contracts/AirDrop721A.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract NFTCONTRACT is ERC721A, Ownable{
+    using Strings for uint256;
+
+    uint256 public constant MAX_SUPPLY = 10000;
+    uint256 public constant MAX_PUBLIC_MINT = 10;
+    uint256 public constant MAX_WHITELIST_MINT = 3;
+    uint256 public constant PUBLIC_SALE_PRICE = .099 ether;
+    uint256 public constant WHITELIST_SALE_PRICE = .0799 ether;
+
+    string private  baseTokenUri;
+    string public   placeholderTokenUri;
+
+    //deploy smart contract, toggle WL, toggle WL when done, toggle publicSale
+    bool public isRevealed;
+    bool public publicSale;
+    bool public whiteListSale;
+    bool public pause;
+    bool public teamMinted;
+
+    bytes32 private merkleRoot;
+
+    mapping(address => uint256) public totalPublicMint;
+    mapping(address => uint256) public totalWhitelistMint;
+
+    constructor() ERC721A("NFTCONTRACT", "CHP"){
+
+    }
+
+    modifier callerIsUser() {
+        require(tx.origin == msg.sender, "NFTCONTRACT :: Cannot be called by a contract");
+        _;
+    }
+
+    function mint(uint256 _quantity) external payable callerIsUser{
+        require(publicSale, "NFTCONTRACT :: Not Yet Active.");
+        require((totalSupply() + _quantity) <= MAX_SUPPLY, "NFTCONTRACT :: Beyond Max Supply");
+        require((totalPublicMint[msg.sender] +_quantity) <= MAX_PUBLIC_MINT, "NFTCONTRACT :: Already minted 3 times!");
+        require(msg.value >= (PUBLIC_SALE_PRICE * _quantity), "NFTCONTRACT :: Below ");
+
+        totalPublicMint[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function whitelistMint(bytes32[] memory _merkleProof, uint256 _quantity) external payable callerIsUser{
+        require(whiteListSale, "NFTCONTRACT :: Minting is on Pause");
+        require((totalSupply() + _quantity) <= MAX_SUPPLY, "NFTCONTRACT :: Cannot mint beyond max supply");
+        require((totalWhitelistMint[msg.sender] + _quantity)  <= MAX_WHITELIST_MINT, "NFTCONTRACT :: Cannot mint beyond whitelist max mint!");
+        require(msg.value >= (WHITELIST_SALE_PRICE * _quantity), "NFTCONTRACT :: Payment is below the price");
+        //create leaf node
+        bytes32 sender = keccak256(abi.encodePacked(msg.sender));
+        require(MerkleProof.verify(_merkleProof, merkleRoot, sender), "NFTCONTRACT :: You are not whitelisted");
+
+        totalWhitelistMint[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function teamMint() external onlyOwner{
+        require(!teamMinted, "NFTCONTRACT :: Team already minted");
+        teamMinted = true;
+        _safeMint(msg.sender, 200);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenUri;
+    }
+
+    //return uri for certain token
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        if(!isRevealed){
+            return placeholderTokenUri;
+        }
+        //string memory baseURI = _baseURI();
+        return bytes(baseTokenUri).length > 0 ? string(abi.encodePacked(baseTokenUri, tokenId.toString(), ".json")) : "";
+    }
+
+    function setTokenUri(string memory _baseTokenUri) external onlyOwner{
+        baseTokenUri = _baseTokenUri;
+    }
+    function setPlaceHolderUri(string memory _placeholderTokenUri) external onlyOwner{
+        placeholderTokenUri = _placeholderTokenUri;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner{
+        merkleRoot = _merkleRoot;
+    }
+
+    function getMerkleRoot() external view returns (bytes32){
+        return merkleRoot;
+    }
+
+    function togglePause() external onlyOwner{
+        pause = !pause;
+    }
+
+    function toggleWhiteListSale() external onlyOwner{
+        whiteListSale = !whiteListSale;
+    }
+
+    function togglePublicSale() external onlyOwner{
+        publicSale = !publicSale;
+    }
+
+    function toggleReveal() external onlyOwner{
+        isRevealed = !isRevealed;
+    }
+
+    function withdraw() external onlyOwner{
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
+
+/* Merkle proof for address 2
+   ["0x5931b4ed56ace4c46b68524cb5bcbf4195f1bbaacbe5228fbd090546c88dd229","0x4726e4102af77216b09ccd94f40daa10531c87c4d60bba7f3b3faf5ff9f19b3c","0xc7348b15532ca2d26bca10e3d3b985a564813cd9a0f710f7acb7ab94ad00da56","0x52625854d95b708b24d2c02807f3b928a535be08a2dc592403087d70791e4ad7"] */
